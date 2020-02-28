e1d4511f2ccb53d37d19a1c530c86c62e71acc35
desislavpetrov <petrov_desislav@abv.bg>
2020-02-28 12:24:47 +0200

    added connection to the database and checking for users in the database

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,6 @@
     <head>
         <meta charset="UTF-8">
-        <title>ImaginaryEnv</title>
+        <title>PHP Content Management System</title>
         <link rel="stylesheet" href="css/bootstrap.css">
         <link rel="stylesheet" href="css/style.css">
     </head>
--- page3.php
+++ page3.php
@@ -1,3 +1,23 @@
+<?php
+//  *************** For PostgreSQL
+$dsn = "pgsql:host=localhost;dbname=login_course;port=5432";
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false
+];
+$pdo = new PDO($dsn, 'postgres', 'express', $opt);
+echo "Connected To Database";
+//  *************** For MySQL
+//    $dsn = "mysql:host=localhost;dbname=login_course;port=3306;charset=utf8";
+//    $opt = [
+//        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+//        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//        PDO::ATTR_EMULATE_PREPARES   => false
+//    ];
+//    $pdo = new PDO($dsn, $user, $pass, $opt);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <?php include "includes/header.php" ?>
@@ -7,8 +27,21 @@
         <div class="container">
             <h1 class="text-center">Page 3</h1>
             <p>"Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+            <br>
+            <?php
+    $result = $pdo->query("SELECT * FROM users");
+    if ($result->rowCount()>0) {
+        foreach ($result as $row) {
+            echo $row['firstname'];
+        }
+    } else {
+        echo "No users in users table";
+    }
+
+            ?>
         </div> <!--Container-->
-        
+
         <?php include "includes/footer.php" ?>
     </body>
 </html>
