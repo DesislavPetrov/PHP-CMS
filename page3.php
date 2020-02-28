0ed6437668334d0901d0decdaecef07c7d2c836f
desislavpetrov <petrov_desislav@abv.bg>
2020-02-28 14:51:28 +0200

    added displaying user data from the database

--- page3.php
+++ page3.php
@@ -1,13 +1,13 @@
 <?php
 //  *************** For PostgreSQL
-$dsn = "pgsql:host=localhost;dbname=login_course;port=5432";
-$opt = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false
-];
-$pdo = new PDO($dsn, 'postgres', 'express', $opt);
-echo "Connected To Database";
+    $dsn = "pgsql:host=localhost;dbname=login_course;port=5432";
+    $opt = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false
+    ];
+    $pdo = new PDO($dsn, 'postgres', 'express', $opt);
+    echo "Connected To Database";
 //  *************** For MySQL
 //    $dsn = "mysql:host=localhost;dbname=login_course;port=3306;charset=utf8";
 //    $opt = [
@@ -27,21 +27,27 @@ echo "Connected To Database";
         <div class="container">
             <h1 class="text-center">Page 3</h1>
             <p>"Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-
+            
             <br>
             <?php
-    $result = $pdo->query("SELECT * FROM users");
-    if ($result->rowCount()>0) {
-        foreach ($result as $row) {
-            echo $row['firstname'];
-        }
-    } else {
-        echo "No users in users table";
-    }
-
+                try {
+                    $result = $pdo->query("SELECT firstname, lastname, username, password FROM users");
+                    if ($result->rowCount()>0) {
+                        echo "<table class='table'>";
+                        echo "<tr><th>Firstname</th><th>Lastname</th><th>Username</th><th>Password</th></tr>";
+                        foreach ($result as $row) {
+                            echo "<tr><td>{$row['firstname']}</td><td>{$row['lastname']}</td><td>{$row['username']}</td><td>{$row['password']}</td></tr>";
+                        }
+                        echo "</table>";
+                    } else {
+                        echo "No users in users table";
+                    }
+                } catch(PDOException $e){
+                    echo "Oops there was an error<br><br>".$e->getMessage();
+                }
             ?>
         </div> <!--Container-->
-
+        
         <?php include "includes/footer.php" ?>
     </body>
 </html>
